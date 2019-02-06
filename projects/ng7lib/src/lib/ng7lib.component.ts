4914f5fe4f8b6f3eb527b3c95185b81aac65e774
dreamerkumar <connectwithvkumar@gmail.com>
2019-02-06 16:06:47 -0500

    include name parser as a whitelisted dependency added commented code to show that * throws error added common module to use json pipe

--- projects/ng7lib/src/lib/ng7lib.component.ts
+++ projects/ng7lib/src/lib/ng7lib.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+// /todo import * as nameparser from 'another-name-parser';
+import nameparser from 'another-name-parser';
 @Component({
   selector: 'lib-ng7lib',
   template: `
     <p>
-      ng7lib works!
+      ng7lib works! Value of name is {{name| json }}
     </p>
   `,
   styles: []
 })
 export class Ng7libComponent implements OnInit {
-
+  name: string;
   constructor() { }
 
   ngOnInit() {
+    this.name = nameparser('Commissioner James "Jim" W. Gordon, Sr.');
   }
 
 }
--- projects/ng7lib/src/lib/ng7lib.module.ts
+++ projects/ng7lib/src/lib/ng7lib.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Ng7libComponent } from './ng7lib.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [Ng7libComponent],
   imports: [
+    CommonModule
   ],
   exports: [Ng7libComponent]
 })
